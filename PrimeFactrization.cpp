f6a5990d4574452bc46903b59e5dc0d7d79b55d6
tottan <t_izaki@outlook.jp>
2022-12-04 14:30:42 +0900

    add

---
+++ PrimeFactrization.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    
+
+    return 0;
+}
+
+/*
+- author: tottan
+- college: University of Hyogo
+- organization: DA研(https://uoh-dakken.com)
+- date: 2022-12-04 14:11:40
+*/
